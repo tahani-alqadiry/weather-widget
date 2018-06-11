8b90b98af2b11013fec2308c4dfad73983c23846
Tahani-alqadiry <tahani-alqsj@hotmail.com>
2018-06-11 05:17:19 +0300

    adding things before injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import {WeatherComponent} from './weather-widget/component/weather.component'
 
 @NgModule({
   declarations: [
+    WeatherComponent,
     AppComponent
   ],
   imports: [
+    AngularFontAwesomeModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/weather-widget/component/weather.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import {WeatherService} from '../service/weather.service';
+@Component({
+    // moduleId:module.id,
+    selector: 'weather-widget',
+    templateUrl: './weather.component.html',
+    styleUrls: ['./weather.component.css'],
+    providers:[WeatherService]
+})
+
+export class WeatherComponent{
+    constructor(private service:WeatherService){
+
+    }
+}
